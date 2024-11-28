db365fe1aa3b9767e00fb0418a7cf4569e0bc61f
ShivamGithub120999 <sainishivam877@gmail.com>
2024-11-28 20:19:36 +0530

    Handle Two Numbers

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -2,8 +2,10 @@ export class StringCalculator {
     add(numbers: string): number {
         if ( !numbers ) return 0;
 
-        return 0;
-        // Handle the single number case
-        return parseInt(numbers, 10);
+        // Split the string by comma
+        const numList = numbers.split(',').map(num => parseInt(num, 10));
+
+        // Sum the numbers and return the result
+        return numList.reduce((sum, num) => sum + num, 0);
     }
 }
